5398b05d574ad6a97baee2b0daf2bebdef96364d
TARUC <wailun_97@live.com.my>
2020-03-03 16:00:37 +0800

    second commit

--- app/src/main/java/com/example/practical_1/MainActivity.kt
+++ app/src/main/java/com/example/practical_1/MainActivity.kt
@@ -13,13 +13,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener{ rollDice() }
+        rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener { countUp() }
     }
+
     private fun rollDice() {
         val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
         resultText.text = randomInt.toString()
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            this, "button clicked",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        if (resultText.text == "Hello World!") {
+            resultText.text == "1"
+        } else {
+            var resultInt = resultText.text.toString().toInt()
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text.toString()
+            }
+        }
     }
 }
